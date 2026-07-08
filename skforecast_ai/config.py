0413a48709ded106c1143686231068acf4cb5a85
Javier Escobar Ortiz <javier.escobar.ortiz@gmail.com>
2026-07-08 11:29:50 +0200

    pyproject.toml

--- skforecast_ai/config.py
+++ skforecast_ai/config.py
@@ -3,8 +3,13 @@
 from __future__ import annotations
 
 import os
-import tomllib
+import sys
 from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
 
 CONFIG_DIR: Path = (
